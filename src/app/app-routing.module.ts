7d4e8cb46d75ad6f3bbee5600d9853da83603600
yuuki-kouno <zgoovw@gmail.com>
2020-01-13 12:06:34 +0900

    feat(welcome): welcomeページ追加

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,11 +1,21 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
-
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: 'welcome',
+    loadChildren: () =>
+      import('./welcome/welcome.module').then(mod => mod.WelcomeModule)
+  },
+  {
+    path: 'create',
+    loadChildren: () =>
+      import('./create/create.module').then(mod => mod.CreateModule)
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
---
+++ src/app/welcome/welcome.module.ts
@@ -0,0 +1,21 @@
+import { NgModule } from "@angular/core";
+import { CommonModule } from "@angular/common";
+
+import { MatButtonModule } from "@angular/material/button";
+import { WelcomeRoutingModule } from "./welcome-routing.module";
+import { WelcomeComponent } from "./welcome/welcome.component";
+import { MatDialogModule } from "@angular/material/dialog";
+import { MatFormFieldModule } from "@angular/material/form-field";
+
+@NgModule({
+  declarations: [WelcomeComponent],
+  imports: [
+    CommonModule,
+    WelcomeRoutingModule,
+    MatButtonModule,
+    MatDialogModule,
+    MatFormFieldModule
+  ],
+  entryComponents: [WelcomeComponent]
+})
+export class WelcomeModule {}
