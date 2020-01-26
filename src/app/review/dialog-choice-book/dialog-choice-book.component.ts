bb524563df52d460dc37d2dd1fd70a406a20e259
yuuki-kouno <zgoovw@gmail.com>
2020-01-26 23:48:08 +0900

    feat(review): 本を選択ボタンのhtml追加

--- src/app/review/dialog-choice-book/dialog-choice-book.component.ts
+++ src/app/review/dialog-choice-book/dialog-choice-book.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { MatDialog } from '@angular/material/dialog';
+
 
 export interface DialogData {
   animal: 'panda' | 'unicorn' | 'lion';
@@ -12,25 +12,10 @@ export interface DialogData {
   styleUrls: ['./dialog-choice-book.component.scss']
 })
 export class DialogChoiceBookComponent implements OnInit {
-  alertDialogResult = '';
 
-  constructor(public matDialog: MatDialog) {}
 
-  onDelete(event: string) {
-    // ダイアログの表示
-    const dialog = this.matDialog.open(DialogChoiceBookComponent, {
-      data: { title: '削除確認', message: '削除しても大丈夫ですか？' },
-      height: '300px',
-      width: '500px',
-      disableClose: false
-    });
+  constructor() {}
 
-    // ボタンの結果を取得
-    dialog.afterClosed().subscribe((result: any) => {
-      // 結果をセット
-      this.alertDialogResult = result;
-    });
-  }
 
   ngOnInit() {}
 }
