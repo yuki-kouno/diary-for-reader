2487619c89ad2b6e7b714ef061a6c8540f4086e7
yuuki-kouno <zgoovw@gmail.com>
2020-03-20 17:05:39 +0900

    feat(firebase): firebase初期化

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,17 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  firebase: {
+    apiKey: 'AIzaSyDQ3iQrweqnebsp52TG9QoWYAYZy4PIU_4',
+    authDomain: 'diary-for-reader.firebaseapp.com',
+    databaseURL: 'https://diary-for-reader.firebaseio.com',
+    projectId: 'diary-for-reader',
+    storageBucket: 'diary-for-reader.appspot.com',
+    messagingSenderId: '938570394016',
+    appId: '1:938570394016:web:81c28bb6d44a9b84f99cc5',
+    measurementId: 'G-5D14ZGN72Y'
+  }
 };
 
 /*
