f893292fbdf15fc23ec1210341e8e0c90a5145ee
yuuki-kouno <zgoovw@gmail.com>
2020-04-26 17:21:13 +0900

    feat(googleBooksApi): データ取得制限を４０に変更

--- src/app/services/google-books-api.service.ts
+++ src/app/services/google-books-api.service.ts
@@ -5,33 +5,30 @@ import { Book } from '../interface/book';
 import { retry, catchError, windowCount } from 'rxjs/operators';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class GoogleBooksApiService {
   apiUrl = 'https://www.googleapis.com/books/v1/volumes';
-  limit = 10;
 
   constructor(private http: HttpClient) {}
 
   httpOptions = {
     headers: new HttpHeaders({
-      'Content-Type': 'application/json'
-    })
+      'Content-Type': 'application/json',
+    }),
   };
 
   getListOfBooks(searchText): Observable<Book> {
-    return this.http.get<Book>(this.apiUrl + '?q=' + searchText).pipe(
-      retry(1),
-      catchError(this.handleError)
-    );
+    return this.http
+      .get<Book>(this.apiUrl + '?q=' + searchText + '&maxResults=40')
+      .pipe(retry(1), catchError(this.handleError));
   }
 
   // HttpClient API get() method => Fetch Specific Book based on ID
   getSpecificBook(id): Observable<Book> {
-    return this.http.get<Book>(this.apiUrl + '/' + id).pipe(
-      retry(1),
-      catchError(this.handleError)
-    );
+    return this.http
+      .get<Book>(this.apiUrl + '/' + id)
+      .pipe(retry(1), catchError(this.handleError));
   }
 
   // Error handling
