4a7e2bfd22b83851584c9320a13a1302a730c0ea
yuuki-kouno <zgoovw@gmail.com>
2020-04-28 20:35:51 +0900

    fix(environment.prod): firebaseエラー修正

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,13 @@
 export const environment = {
-  production: true
+  production: false,
+  firebase: {
+    apiKey: 'AIzaSyDQ3iQrweqnebsp52TG9QoWYAYZy4PIU_4',
+    authDomain: 'diary-for-reader.firebaseapp.com',
+    databaseURL: 'https://diary-for-reader.firebaseio.com',
+    projectId: 'diary-for-reader',
+    storageBucket: 'diary-for-reader.appspot.com',
+    messagingSenderId: '938570394016',
+    appId: '1:938570394016:web:81c28bb6d44a9b84f99cc5',
+    measurementId: 'G-5D14ZGN72Y',
+  },
 };
