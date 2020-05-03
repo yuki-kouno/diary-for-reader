8290391af1ce8a2af89357bf357f9a4375bacc65
yuuki-kouno <zgoovw@gmail.com>
2020-05-03 23:58:40 +0900

    fix(AuthGuard): canActivateが機能するように修正した

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,45 +2,45 @@ import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { AuthGuard } from './guards/auth.guard';
 
-
 const routes: Routes = [
   {
     path: 'welcome',
     loadChildren: () =>
-      import('./welcome/welcome.module').then(mod => mod.WelcomeModule)
+      import('./welcome/welcome.module').then((mod) => mod.WelcomeModule),
   },
   {
-    path: 'review',
+    path: '',
+    pathMatch: 'full',
     loadChildren: () =>
-      import('./review/review.module').then(mod => mod.ReviewModule),
+      import('./review/review.module').then((mod) => mod.ReviewModule),
     canLoad: [AuthGuard],
-    canActivate: [AuthGuard]
+    canActivate: [AuthGuard],
   },
   {
     path: 'library',
     loadChildren: () =>
-      import('./library/library.module').then(mod => mod.LibraryModule),
+      import('./library/library.module').then((mod) => mod.LibraryModule),
     canLoad: [AuthGuard],
-    canActivate: [AuthGuard]
+    canActivate: [AuthGuard],
   },
   {
     path: 'add-books',
     loadChildren: () =>
-      import('./add-books/add-books.module').then(mod => mod.AddBooksModule),
+      import('./add-books/add-books.module').then((mod) => mod.AddBooksModule),
     canLoad: [AuthGuard],
-    canActivate: [AuthGuard]
+    canActivate: [AuthGuard],
   },
   {
     path: 'calendar',
     loadChildren: () =>
-      import('./calendar/calendar.module').then(mod => mod.CalendarModule),
+      import('./calendar/calendar.module').then((mod) => mod.CalendarModule),
     canLoad: [AuthGuard],
-    canActivate: [AuthGuard]
-  }
+    canActivate: [AuthGuard],
+  },
 ];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
 export class AppRoutingModule {}
