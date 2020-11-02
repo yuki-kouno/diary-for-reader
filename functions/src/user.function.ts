c37b5b4462c45bcea01cd76a9d90a540b1293567
yuuki-kouno <zgoovw@gmail.com>
2020-11-03 00:59:58 +0900

    fix(functions): db定義のexport除去

--- functions/src/user.function.ts
+++ functions/src/user.function.ts
@@ -2,7 +2,7 @@ import * as admin from 'firebase-admin';
 import * as functions from 'firebase-functions';
 // import { User } from './interfaces/user';
 
-export const db = admin.firestore();
+const db = admin.firestore();
 
 export const createUser = functions
   .region('asia-northeast1')
