e2c9cd1bf7339c448b0be8d67e35b64fb216f106
yuuki-kouno <zgoovw@gmail.com>
2020-11-25 22:48:19 +0900

    refactor(signupForm): createUser()のimportPath変更

--- src/app/welcome/signup-form/signup-form.component.ts
+++ src/app/welcome/signup-form/signup-form.component.ts
@@ -1,18 +1,17 @@
 import { Component, OnInit } from '@angular/core';
 import { Validators, FormControl, FormBuilder } from '@angular/forms';
 import { MatDialog } from '@angular/material/dialog';
-import { AuthService } from 'src/app/services/auth.service';
-import { Router } from '@angular/router';
+import { UserService } from 'src/app/services/user.service';
 
 @Component({
   selector: 'app-signup-form',
   templateUrl: './signup-form.component.html',
-  styleUrls: ['./signup-form.component.scss']
+  styleUrls: ['./signup-form.component.scss'],
 })
 export class SignupFormComponent implements OnInit {
   signUpForm = this.fb.group({
     email: ['', [Validators.required, Validators.email]],
-    password: ['', [Validators.required]]
+    password: ['', [Validators.required]],
   });
 
   get emailControl() {
@@ -25,8 +24,7 @@ export class SignupFormComponent implements OnInit {
   constructor(
     private fb: FormBuilder,
     private dialog: MatDialog,
-    private authService: AuthService,
-    private router: Router
+    private userService: UserService
   ) {}
 
   closeDialog() {
@@ -34,7 +32,7 @@ export class SignupFormComponent implements OnInit {
   }
 
   signUp() {
-    this.authService.createUser(this.signUpForm.value);
+    this.userService.createUser(this.signUpForm.value);
   }
 
   ngOnInit() {}
