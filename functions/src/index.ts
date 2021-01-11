f044392f46cab39eba35e6ab1d298090b77b447e
yuuki-kouno <zgoovw@gmail.com>
2021-01-11 13:04:46 +0900

    feat(バックアップ): 定期バックアップ設定完了

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -2,3 +2,4 @@ export * from './favorite-book.function';
 export * from './book-ranking-scraping.function';
 export * from './user.function';
 export * from './render.function';
+export { backup } from './backup.function';
