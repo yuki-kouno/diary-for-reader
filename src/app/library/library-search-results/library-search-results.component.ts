18e760ca0a00a303beddb3da2204165714789be9
yuuki-kouno <zgoovw@gmail.com>
2021-02-16 12:29:05 +0900

    fix(class, 関数名): .container共通化, UI修正, 関数名修正

--- src/app/library/library-search-results/library-search-results.component.ts
+++ src/app/library/library-search-results/library-search-results.component.ts
@@ -1,10 +1,4 @@
-import {
-  Component,
-  OnInit,
-  AfterViewInit,
-  ElementRef,
-  OnDestroy,
-} from '@angular/core';
+import { Component, OnInit, OnDestroy } from '@angular/core';
 import { SearchLibraryService } from 'src/app/services/search-library.service';
 import { SearchIndex } from 'algoliasearch/lite';
 import { ActivatedRoute } from '@angular/router';
@@ -22,8 +16,7 @@ import { LoadingService } from 'src/app/services/loading.service';
   templateUrl: './library-search-results.component.html',
   styleUrls: ['./library-search-results.component.scss'],
 })
-export class LibrarySearchResultsComponent
-  implements OnInit, AfterViewInit, OnDestroy {
+export class LibrarySearchResultsComponent implements OnInit, OnDestroy {
   index: SearchIndex = this.searchLibraryService.index.item;
   searchText: string;
   uid: string = this.authService.uid;
@@ -35,7 +28,6 @@ export class LibrarySearchResultsComponent
     private searchLibraryService: SearchLibraryService,
     private route: ActivatedRoute,
     private authService: AuthService,
-    private elementRef: ElementRef,
     private dialog: MatDialog,
     private booksService: DatabaseBooksService,
     private seoService: SeoService,
@@ -51,7 +43,7 @@ export class LibrarySearchResultsComponent
       .afterClosed()
       .subscribe((status) => {
         if (status) {
-          this.booksService.removeToFavoriteBook(book.id);
+          this.booksService.removeFavoriteBook(book.id);
           this.results.splice(i, 1);
         }
       });
@@ -72,11 +64,6 @@ export class LibrarySearchResultsComponent
         }
       });
     });
-  }
-
-  ngAfterViewInit() {
-    this.elementRef.nativeElement.ownerDocument.body.style.background =
-      'rgb(237, 245, 245)';
   }
 
   ngOnDestroy() {
