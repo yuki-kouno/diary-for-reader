753d6a6eb7950d81e7bf33f4a8f3f8ed88f0c8c2
yuki-kouno <zgoovw@gmail.com>
2021-04-28 11:43:50 +0900

    検索画面のツアーbase ui作成

---
+++ src/app/animations/animations.ts
@@ -0,0 +1,5 @@
+// import { transition, trigger } from '@angular/animations';
+
+// export const fade = trigger('fade', [
+//   transition('* => void', [animate('.5s ease-out', style({ opacity: 0 }))]),
+// ]);
