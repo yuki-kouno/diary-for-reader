eca4477b9f0e6e61ad7077fa17e85745bb82fc07
yuki-kouno <zgoovw@gmail.com>
2021-04-29 23:07:14 +0900

    不要フォルダ削除

--- src/app/animations/animations.ts
+++
@@ -1,5 +0,0 @@
-// import { transition, trigger } from '@angular/animations';
-
-// export const fade = trigger('fade', [
-//   transition('* => void', [animate('.5s ease-out', style({ opacity: 0 }))]),
-// ]);
