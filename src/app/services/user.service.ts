15466a4b1acdeaa9139f046736238475653040a8
yuki-kouno <zgoovw@gmail.com>
2021-05-07 19:45:03 +0900

    ビジネスアカウントは退会できないようにする Fixes #58

--- src/app/services/user.service.ts
+++ src/app/services/user.service.ts
@@ -3,11 +3,12 @@ import { User } from '../interface/user';
 import { AngularFirestore } from '@angular/fire/firestore';
 import { AngularFireAuth } from '@angular/fire/auth';
 import { Observable, of } from 'rxjs';
-import { switchMap, first, take } from 'rxjs/operators';
+import { switchMap, first } from 'rxjs/operators';
 import { Router } from '@angular/router';
 import { MatSnackBar } from '@angular/material/snack-bar';
 import { AngularFireFunctions } from '@angular/fire/functions';
 import { AuthService } from './auth.service';
+import { LoadingService } from './loading.service';
 
 @Injectable({
   providedIn: 'root',
@@ -25,15 +26,14 @@ export class UserService {
     })
   );
 
-  isProcessing: boolean;
-
   constructor(
     private db: AngularFirestore,
     private afAuth: AngularFireAuth,
     private router: Router,
     private snackBar: MatSnackBar,
     private fns: AngularFireFunctions,
-    private authService: AuthService
+    private authService: AuthService,
+    private loadingSercvice: LoadingService
   ) {}
 
   getUserByID(uid: string): Observable<User> {
@@ -45,23 +45,43 @@ export class UserService {
   }
 
   async deleteUser(): Promise<void> {
-    this.isProcessing = true;
-    const callable = this.fns.httpsCallable('deleteAfUser');
-    return callable(this.authService.uid)
-      .toPromise()
-      .then(() => {
-        this.router.navigateByUrl('/welcome');
-        this.snackBar.open('ご利用ありがとうございました');
-      })
-      .catch((err) => {
-        console.log(err);
-        this.snackBar.open(
-          '削除に失敗しました。もう一度退会を実行してください'
-        );
-      })
-      .finally(() => {
-        this.isProcessing = false;
-      });
+    if (this.authService.uid === 'V4RMoLLIeFVzS0GCj7nOU2OIHXQ2') {
+      this.loadingSercvice.loading = true;
+      const callable = this.fns.httpsCallable('resetGuestDataByClient');
+      return callable(this.authService.uid)
+        .toPromise()
+        .then(() => {
+          this.router.navigateByUrl('/welcome');
+          this.snackBar.open('ご利用ありがとうございました');
+        })
+        .catch((err) => {
+          console.log(err);
+          this.snackBar.open(
+            '削除に失敗しました。もう一度退会を実行してください'
+          );
+        })
+        .finally(() => {
+          this.loadingSercvice.loading = false;
+        });
+    } else {
+      this.loadingSercvice.loading = true;
+      const callable = this.fns.httpsCallable('deleteAfUser');
+      return callable(this.authService.uid)
+        .toPromise()
+        .then(() => {
+          this.router.navigateByUrl('/welcome');
+          this.snackBar.open('ご利用ありがとうございました');
+        })
+        .catch((err) => {
+          console.log(err);
+          this.snackBar.open(
+            '削除に失敗しました。もう一度退会を実行してください'
+          );
+        })
+        .finally(() => {
+          this.loadingSercvice.loading = false;
+        });
+    }
   }
 
   updateUserTour(value: any): void {
